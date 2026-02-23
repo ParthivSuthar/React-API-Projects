99754c3041445afb08f1eef4b4c7c909937f043d
Parthiv Suthar <myworkflow7070@gmail.com>
2026-02-23 22:29:58 +0530

    create Hero component for adding hero image

---
+++ Instamart/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='w-full'>
+        <img src="https://instamart-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_800/MERCHANDISING_BANNERS/IMAGES/MERCH/2026/2/13/0d2fd5b7-74de-4a44-83d5-20031db2c58d_Dinner Ready.png" className='mx-auto' alt="hero" />
+    </div>
+  )
+}
+
+export default Hero
