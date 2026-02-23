fda7daeb7c82c4d13d6910d68f3a4e4d89c5ae00
Parthiv Suthar <myworkflow7070@gmail.com>
2026-02-23 22:28:28 +0530

    create file for adding constants resources

---
+++ Instamart/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const LOGO_URL = "https://instamart-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto//InstamartAssets/DWeb%20Changes/Instamart%20log.png"
+
+export const ASSET_URL = "https://instamart-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200/"
