8f4228d1ccff1d5cf5dfbcffc19ac8752fa02c52
Parthiv Suthar <myworkflow7070@gmail.com>
2026-02-25 23:22:53 +0530

    configure routing for home and displaying lists under category based on selected category name

--- Instamart/src/App.jsx
+++ Instamart/src/App.jsx
@@ -1,6 +1,8 @@
 import Body from "./components/Body"
+import CategoryName from "./components/CategoryName"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom"
 
 
 function App() {
@@ -9,10 +11,15 @@ function App() {
   return (
     <div className="w-full">
     <Header />
-    <Hero />
-    <Body />
+    <Outlet />
+    {/* <Hero />
+    <Body /> */}
     </div>
   )
 }
+
+
+
+
 
 export default App
