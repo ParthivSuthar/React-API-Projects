20df9e7cadf1f04cee10a9bb731988db8f3fe1fd
Parthiv Suthar <myworkflow7070@gmail.com>
2026-02-25 23:25:48 +0530

    Add here configuration for routing

--- Instamart/src/main.jsx
+++ Instamart/src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Body from './components/Body.jsx'
+import CategoryName from './components/CategoryName.jsx'
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/', 
+    element: <App />,
+    children: [
+      {
+        path:"/", 
+        element: <Body />
+      },
+      {
+        path:"/category/:categoryname", 
+        element: <CategoryName />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <RouterProvider router={appRouter}>
     <App />
-  </StrictMode>,
+  </RouterProvider>,
 )
